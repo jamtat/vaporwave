3ee88f8d7b6831e3f96fed0206ef4ba8b45b08e4
Ben Tattersley <benjamintattersley@gambitresearch.com>
2017-04-03 18:07:12 +0100

    INITIAL MEME

---
+++ index.js
@@ -0,0 +1,17 @@
+'use memes'
+
+const charToFullWidth = char => {
+	const c = char.charCodeAt( 0 )
+	return c >= 33 && c <= 126
+		? String.fromCharCode( ( c - 33 ) + 65281 )
+		: char
+}
+
+const stringToFullWidth = string => string.split( '' ).map( charToFullWidth ).join( '' )
+
+module.exports = stringToFullWidth
+
+process.stdin.resume( )
+process.stdin.setEncoding( 'utf8' )
+
+process.stdin.on( 'data', chunk => process.stdout.write( stringToFullWidth( chunk ) ) )
